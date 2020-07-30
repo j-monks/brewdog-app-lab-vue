98386a4c93c0662bf190b9d0b89bc7e437a7f1cb
James Monks <J.monks23@gmail.com>
2020-07-30 12:01:58 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import BeerList from './components/BeerList.vue'
+// import BeerDetail from './components/BeerDetail.vue'
+// import FavouriteList from './components/FavouriteList.vue'
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      beers: [],
+      selectedBeer: null
+    }
+  },
   components: {
-    HelloWorld
+      // "beer-list": BeerList,
+      // "beer-detail": BeerDetail,
+    },
+  mounted() {
+      fetch("https://api.punkapi.com/v2/beers")
+        .then(result => result.json())
+        .then(beers => this.beers = beers )
+    }
   }
-}
+
+
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
